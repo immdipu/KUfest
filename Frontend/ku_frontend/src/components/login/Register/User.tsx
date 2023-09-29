deef794be688c004b66e0e772f5caa470d633ee2
Rising Star <incredibleitsme404@gamil.com>
2023-09-29 21:06:24 +0545

    .

--- Frontend/ku_frontend/src/components/login/Register/User.tsx
+++ Frontend/ku_frontend/src/components/login/Register/User.tsx
@@ -78,7 +78,117 @@ const User = () => {
     console.log(usersform);
   };
 
-  return (
+const countries = [
+  'Afghanistan',
+  'Albania',
+  'Algeria',
+  'Argentina',
+  'Armenia',
+  'Australia',
+  'Austria',
+  'Bahrain',
+  'Bangladesh',
+  'Belgium',
+  'Bhutan',
+  'Brazil',
+  'Bulgaria',
+  'Burundi',
+  'Cambodia',
+  'Cameroon',
+  'Canada',
+  'Central African Republic',
+  'Chile',
+  'China',
+  'Colombia',
+  'Costa Rica',
+  'Croatia',
+  'Cyprus',
+  'Denmark',
+  'Ecuador',
+  'Egypt',
+  'Ethiopia',
+  'Finland',
+  'France',
+  'Georgia',
+  'Germany',
+  'Ghana',
+  'Greece',
+  'Haiti',
+  'Hungary',
+  'Iceland',
+  'India',
+  'Indonesia',
+  'Iran',
+  'Iraq',
+  'Ireland',
+  'Italy',
+  'Jamaica',
+  'Japan',
+  'Jordan',
+  'Kazakhstan',
+  'Kenya',
+  'Lebanon',
+  'Liberia',
+  'Malaysia',
+  'Maldives',
+  'Mali',
+  'Malta',
+  'Mexico',
+  'Mongolia',
+  'Morocco',
+  'Myanmar (formerly Burma)',
+  'Nepal',
+  'Netherlands',
+  'New Zealand',
+  'Niger',
+  'Nigeria',
+  'North Korea',
+  'Norway',
+  'Oman',
+  'Pakistan',
+  'Panama',
+  'Papua New Guinea',
+  'Paraguay',
+  'Peru',
+  'Philippines',
+  'Poland',
+  'Portugal',
+  'Qatar',
+  'Romania',
+  'Russia',
+  'Saudi Arabia',
+  'Senegal',
+  'Serbia',
+  'Singapore',
+  'Slovakia',
+  'Slovenia',
+  'Somalia',
+  'South Africa',
+  'South Korea',
+  'South Sudan',
+  'Spain',
+  'Sri Lanka',
+  'Sudan',
+  'Sweden',
+  'Switzerland',
+  'Syria',
+  'Tajikistan',
+  'Tanzania',
+  'Thailand',
+  'Tunisia',
+  'Turkey',
+  'Turkmenistan',
+  'Uganda',
+  'Ukraine',
+  'United Arab Emirates',
+  'United Kingdom',
+  'United States of America',
+  'Uruguay',
+  'Venezuela',
+  'Vietnam'
+];
+
+return(
     <div>
       <h3 className="text-center mt-16 text-xl font-semibold text-green-600">
         Join Our Inclusive Tourism Community as a Tourist
@@ -204,6 +314,26 @@ const User = () => {
           <Separator />
           <br />
           <section>
+          <div className="flex flex-col w-full ">
+              <label htmlFor="country" className="px-1">
+                Your Country :
+              </label>
+              <div className="mt-2 w-full ">
+              <Select>
+               <SelectTrigger className="w-full">
+                <SelectValue placeholder="Select a country" />
+               </SelectTrigger>
+               <SelectContent>
+                 {countries.map((country) => (
+                 <SelectItem key={country} value={country}>
+                 {country}
+                 </SelectItem>
+                 ))}
+               </SelectContent>
+              </Select>
+              </div>
+          </div>
+          <br/>
             <div>
               <label htmlFor="gender" className="px-1">
                 Add languages you can speak :
