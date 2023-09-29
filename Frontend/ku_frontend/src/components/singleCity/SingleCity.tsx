6331aca8da9518b6cbc68cfb31d7315c37eb5e34
Bijaya Poudel <poudelbijaya25@gmail.com>
2023-09-30 00:32:12 +0545

    About Us Added!

--- Frontend/ku_frontend/src/components/singleCity/SingleCity.tsx
+++ Frontend/ku_frontend/src/components/singleCity/SingleCity.tsx
@@ -11,12 +11,12 @@ const SingleCity = () => {
                     className="w-full h-full"
                 />
                 <div className='flex justify-center'>
-                    <p className=' text-white text-center mt-6 md:w-1/2 p-8 bg-black bg-opacity-75'>
+                    <p className='  text-center mt-6 md:w-1/2 p-8  bg-opacity-75'>
                         Pokhara is such a valley in Nepal where Phewa Lake, secluded by the Sarangkote Hills, carefully embraces the reflections of silver crowned Annapurna range in an sublime tranquility. With its breathtaking natural beauty and a plethora of exciting activities, it's no wonder that this charming city is a must -visit destination in Nepal. One can explore the serene Phewa Lake, embark on Annapurna treks, visit the World Peace Pagoda for panaromic views, and discover the cultural heritage at Bindabasini Temple. Get your heart racing with paragliding, zip-lining, rafting and delve into the mysterious Gupteshwor Cave or the intriguing Bat Cave. We can explore the local culture at the Gurkha Memorial Museum and International Mountain Meseum and unwind in Lakeside&apos;uis charming cafes.
                         Visit to be captivated by the magic of this Himalayan gem. End your day by the lake, watching the sun dip behind the mountains, casting a warm, golden glow on Pokhara.
                     </p>
                 </div>
-                <div className='flex justify-center'>
+                <div className='flex justify-center  opacity-75 '>
                     <ol>
                         <li className='list-disc'>Pokhara is a picturesque valley located in Nepal.</li>
                         <li className='list-disc' >It is famous for Phewa Lake, where the Annapurna range is beautifully reflected.</li>
